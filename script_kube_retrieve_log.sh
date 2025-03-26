c2db6a53062201fe204b87d60aa26e6c1735269a
Mathis C. <coudert_mathis@outlook.fr>
2025-03-26 15:33:03 +0100

    Add all the projet into Github

---
+++ script_kube_retrieve_log.sh
@@ -0,0 +1,9 @@
+# Exporting logs
+DATE=$(date +"%d-%m-%y")
+POD=$(kubectl --context=production --namespace "$NAMESPACE" get po |grep "$MY_APP" |awk '{print $1}')
+kubectl --namespace $NAMESPACE logs --since=1m $POD --context=production >> /var/log/$MY_APP-logs_$DATE.log
+
+# Cleanup
+YESTERDAY_DATE=$(date --date=' 1 days ago' '+%d-%m-%y')
+YESTERDAY_FILE="/var/log/$MY_APP-logs_$YESTERDAY_DATE.log"
+rm $YESTERDAY_FILE 2> /dev/null
